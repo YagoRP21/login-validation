8852de94f20ee692cc98c1015fa825a538db096c
YagoRP <yagoribeiropereira21@gmail.com>
2022-10-10 22:51:59 -0300

    adding some changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 350px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     fontFamily: {
       sans: ['Nunito', 'sans-serif'],
     },
